a322b3bb2769bfc5b0bfcb65b9b6452892cf632b
Nihar Chandurkar <chandurkar.nihar@gmail.com>
2018-05-13 17:06:19 +0530

    ignore node_modules folder to sync

--- NPM/npm-demo/index.js
+++ NPM/npm-demo/index.js
@@ -1,11 +1,14 @@
 
 var _ = require('underscore');
+var mongoose = require('mongoose');
+
 
 // how NPM finds module 
 // 1. core module
 // 2. file or folder 
 // 3.  node_module
 
+var mResult = mongoose.Collection()
 
 var result = _.contains([1,2,4], 4);
 
